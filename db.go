cf7350789353b32204d1f2b51f68f825b53d926f
zlyuancn <zlyuancn@163.com>
2025-12-16 12:23:11 +0800

    增加手动初始化能力

--- db.go
+++ db.go
@@ -1,6 +1,8 @@
 package redis_tool
 
 import (
+	"sync/atomic"
+
 	"github.com/zly-app/component/redis"
 	"github.com/zly-app/zapp"
 	"github.com/zly-app/zapp/core"
@@ -9,12 +11,29 @@ import (
 
 var RedisClientName = "redis_tool"
 
+var isManualInit = false
+var onceInit int32 // 只能初始化一次
+
 func GetRedis() (redis.UniversalClient, error) {
 	return redis.GetClient(RedisClientName)
 }
 
 func init() {
 	zapp.AddHandler(zapp.AfterInitializeHandler, func(app core.IApp, handlerType handler.HandlerType) {
-		tryInjectCode()
+		if isManualInit {
+			return
+		}
+		ManualInit()
 	})
+}
+
+// 设置为手动初始化, 必须在 zapp.New 之前调用才有效
+func SetManualInit() {
+	isManualInit = true
+}
+
+func ManualInit() {
+	if atomic.AddInt32(&onceInit, 1) == 1 {
+		tryInjectCode()
+	}
 }
